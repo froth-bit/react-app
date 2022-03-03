cbe0e7c46311b8ffc14e12ddc00e19e481cb62e0
MofFoam <2397521545@qq.com>
2022-03-03 17:37:14 +0800

    11

--- src/conpnent/HelloWorld.js
+++ src/conpnent/HelloWorld.js
@@ -1,7 +1,8 @@
 import {Component, createRef} from "react";
+import {unmountComponentAtNode} from "react-dom";
 
 class MyReactClass extends Component{
-    state = {isHot:true}
+    state = {isHot:true,opacity:1}
 
     changeWeather = ()=>{
         console.log('@',this.myRef.current)
@@ -11,7 +12,27 @@ class MyReactClass extends Component{
 
     myRef = createRef();
 
+    death = ()=>{
+        unmountComponentAtNode(document.getElementById("root"))
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(()=>{
+            let {opacity} = this.state;
+            opacity -= 0.1;
+            if (opacity<=0){
+                opacity = 1;
+            }
+            this.setState({opacity})
+        },200)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
+
     render() {
+
         const {name,age,sex} = this.props;
         let isHot = this.state.isHot;
         return (
@@ -23,6 +44,8 @@ class MyReactClass extends Component{
                 </ul>
                 <h2 ref={this.myRef}>今天天气很{isHot?'炎热':'凉爽'}</h2>
                 <button onClick={this.changeWeather}>点击改变天气</button>
+                <h3 style={{opacity:this.state.opacity}}>学不会啊！</h3>
+                <button onClick={this.death}>删除组件</button>
             </div>
         )
     }
